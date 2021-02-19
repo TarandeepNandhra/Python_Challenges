20b2625e9589e3d5ed672a593e4e94105c021efc
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-02-19 14:24:29 +0000

    Sudoku solver - bugs

--- sudoku_solution_validator.py
+++ sudoku_solution_validator.py
@@ -1,4 +1,4 @@
-# Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, 
+# Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board,
 # and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's,
 # which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
 # The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
@@ -6,20 +6,20 @@
 # Example input
 
 # board = [
-# [1, 3, 2, 5, 7, 9, 4, 6, 8], 
-# [4, 9, 8, 2, 6, 1, 3, 7, 5], 
-# [7, 5, 6, 3, 8, 4, 2, 1, 9], 
-# [6, 4, 3, 1, 5, 8, 7, 9, 2], 
-# [5, 2, 1, 7, 9, 3, 8, 4, 6], 
-# [9, 8, 7, 4, 2, 6, 5, 3, 1], 
-# [2, 1, 4, 9, 3, 5, 6, 8, 7], 
-# [3, 6, 5, 8, 1, 7, 9, 2, 4], 
+# [1, 3, 2, 5, 7, 9, 4, 6, 8],
+# [4, 9, 8, 2, 6, 1, 3, 7, 5],
+# [7, 5, 6, 3, 8, 4, 2, 1, 9],
+# [6, 4, 3, 1, 5, 8, 7, 9, 2],
+# [5, 2, 1, 7, 9, 3, 8, 4, 6],
+# [9, 8, 7, 4, 2, 6, 5, 3, 1],
+# [2, 1, 4, 9, 3, 5, 6, 8, 7],
+# [3, 6, 5, 8, 1, 7, 9, 2, 4],
 # [8, 7, 9, 6, 4, 2, 1, 3, 5]
 # ]
 
 # > False
 
-def check_row(arr, start):
+def check_row_of_squares(arr, start):
   sum = 0
   for j in range(9):
     for i in range(start, start + 3):
@@ -30,29 +30,29 @@ def check_row(arr, start):
         else:
           sum = 0
   return True
-  
+
 def check_rows(board):
     for i in range(1, 10):
         for j in range(9):
             if i not in board[j]:
                 return False
-                
+
     return True
-  
+
 def check_columns(board):
     1_to_9 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i in range(9):
         numbers = []
-    
+
         for j in range(9):
             numbers.push(board[i][j])
-    
+
         if numbers != 1_to_9:
             return False
-  
+
     return True
-  
+
 def check_board():
-  return check_row(board, 0) and check_row(board, 3) and check_row(board, 6) and check_rows(board) and check_columns(board)
-  
+  return check_row_of_squares(board, 0) and check_row_of_squares(board, 3) and check_row_of_squares(board, 6) and check_rows(board) and check_columns(board)
+
 # check_board()
---
+++ sudoku_solver.py
@@ -0,0 +1,59 @@
+puzzle = [[5,3,0,0,7,0,0,0,0],
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
+
+# sudoku(puzzle)
+# # Should return
+#  [[5,3,4,6,7,8,9,1,2],
+#   [6,7,2,1,9,5,3,4,8],
+#   [1,9,8,3,4,2,5,6,7],
+#   [8,5,9,7,6,1,4,2,3],
+#   [4,2,6,8,5,3,7,9,1],
+#   [7,1,3,9,2,4,8,5,6],
+#   [9,6,1,5,3,7,2,8,4],
+#   [2,8,7,4,1,9,6,3,5],
+#   [3,4,5,2,8,6,1,7,9]]
+
+def sudoku(puzzle):
+    for y in range(9):
+        for x in range(9):
+            if puzzle[y][x] == 0:
+                for n in range(1, 10):
+                    if possible(puzzle, y, x, n):
+                        puzzle[y][x] = n
+                        solver(puzzle)
+                        # if possible returns false (dead end) will return here
+                        puzzle[y][x] = 0
+                # if this return is reached, algorithm has tried 1-9 for [y][x],
+                # but has backtracked or possible returned False.
+                return
+
+    return puzzle
+
+def possible(puzzle, y, x, n):
+    # check the row
+    for i in range(9):
+        if puzzle[y][i] == n:
+            return False
+
+    # check the column
+    for i in range(9):
+        if puzzle[i][x] == n:
+            return False
+
+    # check 3x3 square containing [y][x]
+    # coords of top left ele in 3x3 square using floor div
+    y0 = (y // 3) * 3
+    x0 = (x // 3) * 3
+    for i in range(y0, y0 + 3):
+        for j in range(x0, x0 + 3):
+            if puzzle[i][j] == n:
+                return False
+    # puzzle[y][x] could be n as n is not in row, col or square.
+    return True
